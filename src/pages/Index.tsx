dcdf772b7befb0f850e28554663fb81abfe80338
Sudharma <sudharmasuvarna@gmail.com>
2025-08-27 15:01:20 +0000

    commit

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -3,6 +3,7 @@ import Navigation from '@/components/Navigation'
 import Hero from '@/components/Hero'
 import About from '@/components/About'
 import Projects from '@/components/Projects'
+import Skills from '@/components/Skills'
 import Contact from '@/components/Contact'
 
 const Index = () => {
@@ -13,6 +14,7 @@ const Index = () => {
       <main>
         <Hero />
         <About />
+        <Skills/>
         <Projects />
         <Contact />
       </main>
